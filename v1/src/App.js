58238e6755988712ec0374c73e2793b50a4d1ed8
agunghanifan <agung.hanifan@gmail.com>
2021-05-03 14:53:27 +0700

    add react-router-dom, settings router page on app.js and add some pages for app

--- v1/src/App.js
+++ v1/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import { Login, Register, Dashboard, NotFound } from './pages/'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Redirect to ='/v1' />
+        </Route>
+        <Route exact path='/v1'>
+          <Login />
+        </Route>
+        <Route path='/v1/register'>
+          <Register />
+        </Route>
+        <Route path='/v1/dashboard'>
+          <Dashboard />
+        </Route>
+        <Route path='*'>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ v1/src/pages/index.js
@@ -0,0 +1,11 @@
+import Login from './Login'
+import Register from './Register'
+import Dashboard from './Dashboard'
+import NotFound from './NotFound'
+
+export {
+  Login,
+  Register,
+  Dashboard,
+  NotFound
+}
